6a77714561be102bbe129356e3e45f30be472c06
ysawiris <youssef.sawiris@students.makeschool.com>
2020-06-10 18:26:40 -0700

    add weather api

---
+++ data/weather-db.js
@@ -0,0 +1,27 @@
+/* Mongoose Connection */
+const mongoose = require("mongoose");
+assert = require("assert");
+
+const url = "mongodb://user:password@mongo:27017/weather-db?retryWrites=false&authSource=admin";
+mongoose.Promise = global.Promise;
+
+
+let connect = ()=> {
+    console.log("attempting connection");
+    mongoose.connect(
+        url, { useNewUrlParser: true },
+        function(err, db) {
+            assert.equal(null, err);
+            console.log("Connected successfully to database");
+    
+            // db.close(); turn on for testing
+        }
+    );
+    mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
+    mongoose.set("debug", true);
+    
+}
+
+// let timeout =  setTimeout(connect, 50000);
+
+module.exports = mongoose.connection;
---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var weather = require('openweather-apis');
+ 
+weather.setLang('it');
+// English - en, Russian - ru, Italian - it, Spanish - es (or sp),
+// Ukrainian - uk (or ua), German - de, Portuguese - pt,Romanian - ro,
+// Polish - pl, Finnish - fi, Dutch - nl, French - fr, Bulgarian - bg,
+// Swedish - sv (or se), Chinese Tra - zh_tw, Chinese Sim - zh (or zh_cn),
+// Turkish - tr, Croatian - hr, Catalan - ca
+
+
+// set city by name
+weather.setCity('Fairplay');
+// or set the coordinates (latitude,longitude)
+weather.setCoordinate(50.0467656, 20.0048731);
+// or set city by ID (recommended by OpenWeatherMap)
+weather.setCityId(4367872);
+
+// or set zip code
+weather.setZipCode(33615);
+
+// 'metric'  'internal'  'imperial'
+weather.setUnits('metric');
+
+// check http://openweathermap.org/appid#get for get the APPID
+weather.setAPPID('b413f9c129c693c029efc0195b97bb24');
+
+
+
+
+/* GET home page. */
+router.get('/:zipcode', function(req, res, next) {
+  // or set zip code
+  weather.setZipCode(req.params.zipcode);
+  // get all the JSON file returned from server (rich of info)
+  weather.getAllWeather(function(err, JSONObj){
+      console.log(JSONObj);
+  });
+  res.render('index.hbs');
+});
+
+
+module.exports = router;
